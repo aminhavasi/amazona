6886edb9ac178c327b680da337ff30f3cda98b79
amin havasi <pau.ahq@gmail.com>
2020-07-07 16:52:18 +0430

    qt page prodouct

--- frontend/src/Screens/ProductScreen.js
+++ frontend/src/Screens/ProductScreen.js
@@ -7,7 +7,7 @@ const ProductScreen = (props) => {
     const product = data.products.find((x) => x._id === props.match.params.id);
     return (
         <div>
-            <div>
+            <div className="back-to-result">
                 <Link to="/">back to result</Link>
             </div>
             <div className="details">
@@ -16,7 +16,39 @@ const ProductScreen = (props) => {
                 </div>
                 <div className="details-info">
                     <ul>
-                        <li>{product.name}</li>
+                        <li>
+                            <h4>{product.name}</h4>
+                        </li>
+                        <li>
+                            {product.rating}Stars({product.numReviews} Reviews)
+                        </li>
+                        <li>
+                            Price: $<b>{product.price}</b>
+                        </li>
+                        <li>
+                            Description:
+                            <div>{product.description}</div>
+                        </li>
+                    </ul>
+                </div>
+                <div className="details-action">
+                    <ul>
+                        <li>Price:{product.price}</li>
+                        <li>Status:{product.status}</li>{' '}
+                        <li>
+                            Qty:
+                            <select>
+                                <option>1</option>
+                                <option>2</option>
+                                <option>3</option>
+                                <option>4</option>
+                            </select>
+                        </li>
+                        <li>
+                            <button className="button primary">
+                                Add to Cart
+                            </button>
+                        </li>
                     </ul>
                 </div>
             </div>
